f294c34a8967eb4d4efc0b4e0928ce68e3899f7e
Yaakov <git@yaakov.dev>
2024-08-11 18:01:11 +1000

    remove unused imports

--- scream.c
+++ scream.c
@@ -1,12 +1,4 @@
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/fs.h>
-#include <linux/cdev.h>
-#include <linux/device.h>
-#include <linux/kernel.h>
-#include <linux/uaccess.h>
 #include <linux/miscdevice.h>
-#include <linux/stat.h>
 
 MODULE_AUTHOR("Yaakov");
 MODULE_DESCRIPTION("Scream Device");
